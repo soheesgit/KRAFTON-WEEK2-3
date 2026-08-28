a03356ba3c244695bd8a012d32787f12f23009e1
soheesgit <thgml89567@smail.kongju.ac.kr>
2026-08-28 11:38:33 +0900

    basic 1번 문제풀이

--- week2/1. basic/01_string.py
+++ week2/1. basic/01_string.py
@@ -34,16 +34,16 @@ def is_palindrome(s):
     Returns:
         bool: 회문이면 True, 아니면 False
     """
-    # TODO: 알파벳과 숫자만 남기고 소문자로 변환하세요
-    # 힌트: isalnum() 메서드와 lower() 메서드 사용
-    pass
+    lower_string = s.lower()
+    answer = ""
+
+    for a in lower_string:
+        if a.isalnum(): answer += a
+
+    if answer == answer[::-1]:
+        return True
     
-    # TODO: 정제된 문자열이 회문인지 확인하세요
-    # 방법1: 문자열을 뒤집어서 비교 ([::-1] 사용)
-    # 방법2: 양 끝 인덱스를 이용한 투 포인터 방식
-    pass
-    
-    #return False
+    return False
 
 # 테스트 케이스
 if __name__ == "__main__":
