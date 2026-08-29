04d74ec5d140197f041be29e8bc141b433794852
soheesgit <thgml89567@smail.kongju.ac.kr>
2026-08-29 21:58:39 +0900

    Queue 문제 풀이

--- week2/1. basic/13_queue.py
+++ week2/1. basic/13_queue.py
@@ -1,3 +1,4 @@
+from collections import deque
 """
 [큐 - 프린터 대기열]
 
@@ -36,16 +37,15 @@ def process_print_queue(jobs):
     Returns:
         처리된 작업 리스트
     """
-    # TODO: deque로 큐 생성
-    queue = deque(jobs)
+    myQueue = deque(jobs)
     
     processed = []
     
-    # TODO: 큐가 비어있지 않은 동안 반복
-    ## 큐에서 작업 꺼내기
-    ## 작업 처리 (출력 및 리스트에 추가)
-    pass
-    
+    while myQueue:
+        job = myQueue.popleft()
+
+        print(f"처리: {job}")
+        processed.append(job)
     return processed
 
 # 테스트 케이스
