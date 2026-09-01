f6480b1bf5dd1702c2c8876e369f5607dca0efe1
soheesgit <thgml89567@smail.kongju.ac.kr>
2026-09-01 15:20:59 +0900

    하노이 탑(재귀) 문제 풀이

--- week2/2. advanced/04_hanoi_tower.py
+++ week2/2. advanced/04_hanoi_tower.py
@@ -59,8 +59,7 @@
 
 def hanoi_count(n: int) -> int:
     """N 개의 원반을 옮기는 데 필요한 최소 이동 횟수( = 2^N - 1) 를 반환"""
-    # TODO: 2^N - 1 을 정수로 반환하세요.
-    pass
+    return 2 ** n - 1
 
 
 def hanoi_moves(n: int) -> list:
@@ -71,10 +70,30 @@ def hanoi_moves(n: int) -> list:
 
     예) hanoi_moves(2) == [(1, 2), (1, 3), (2, 3)]
     """
-    # TODO: N > 20 또는 N == 0 인 경우 [] 를 반환하세요.
-    # TODO: 그 외에는 재귀로 이동 순서를 만들어 반환하세요.
-    pass
+    
+    result = []
 
+    
+    # 만약 0개 혹은 20개 초과이라면 빈 리스트 [] 반환
+    if n == 0 or n > 20:
+        return result
+
+    def move(k, src, via, dst):
+        # K: 옮겨야 하는 원반의 수
+        # src: 지금 원반들이 있는 기둥
+        # via = 현재 출발지와 목적지가 아닌, 남은 기둥
+        # dst = 지금 보내려는 곳
+        
+        if k == 1:
+            result.append((src, dst)) 
+        else:
+            move(k-1, src, dst, via) # k-1개는 임시 기둥으로 옮기기
+            result.append((src, dst)) 
+            move(k-1, via, src, dst) # 임시 기둥에 있는 k-1개를 다시 옮겨주기
+
+    move(n, 1, 2, 3)
+
+    return result
 
 if __name__ == "__main__":
     print("[테스트 1] N=0 (원반 없음, 옮길 것 없음)")
