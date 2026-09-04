de97d4db67a52fb33a40c48b49368c3a96c9e825
soheesgit <144293475+soheesgit@users.noreply.github.com>
2026-09-04 21:04:36 +0900

    깊이 우선 탐색 구현

--- week3/1. basic/05_dfs.py
+++ week3/1. basic/05_dfs.py
@@ -42,15 +42,15 @@ def dfs(graph, start, visited=None):
     Returns:
         방문 순서 리스트
     """
-    # TODO: visited가 None이면 초기화
-    pass
-    
-    # TODO: 현재 정점 방문
-    pass
-    
-    # TODO: 인접한 정점들에 대해 재귀
-    ## 방문하지 않은 정점이면 재귀 호출
-    pass
+    if visited is None:
+        visited = []
+
+    visited.append(start)
+
+    current = graph[start]
+    for i in current:
+        if i not in visited:
+            dfs(graph, i, visited)
     
     return visited
 
