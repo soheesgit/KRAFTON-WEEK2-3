1bb2fcd36ca5b5afb1e40269b114c4105b978c2c
soheesgit <144293475+soheesgit@users.noreply.github.com>
2026-09-09 15:58:14 +0900

    최장 공통 부분수열 (DP) 문제 풀이

--- week3/2. advanced/02_lcs.py
+++ week3/2. advanced/02_lcs.py
@@ -36,12 +36,20 @@ def lcs_length(s1: str, s2: str) -> int:
     s1, s2 의 가장 긴 공통 부분수열의 길이를 반환.
     어느 한쪽이라도 비어 있으면 0 을 반환합니다.
     """
-    # TODO: 빈 문자열 처리
-    # TODO: (len(s1)+1) x (len(s2)+1) 크기의 2차원 dp 배열을 0 으로 초기화
-    # TODO: 이중 반복문으로 점화식에 따라 dp 채우기
-    # TODO: dp[len(s1)][len(s2)] 반환
-    pass
+    if len(s1) == 0 or len(s2) == 0:
+        return 0
 
+    dp = [[0] * (len(s2) + 1) for _ in range(len(s1) + 1)]
+
+
+    for i in range(1, len(s1) + 1):
+        for j in range(1, len(s2) + 1):
+            if s1[i-1] == s2[j-1]:
+                dp[i][j] = dp[i-1][j-1] + 1
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+    return dp[len(s1)][len(s2)]
 
 if __name__ == "__main__":
     print("[테스트 1] 한쪽이 빈 문자열")
